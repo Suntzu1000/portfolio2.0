e6636dd8f47fd21b9a7bd4f829ec8b21d7c465cb
Gabriel Andrade <99212400+Suntzu1000@users.noreply.github.com>
2023-02-09 10:58:23 -0300

    Estilizando o começo

--- portfolio-starter/src/components/Banner.js
+++ portfolio-starter/src/components/Banner.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "../assets/avatar.svg";
+import {Link} from 'react-scroll'
 
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 
@@ -12,29 +13,37 @@ const Banner = () => {
   return (
     <section className="section" id="home">
       <div className="container mx-auto">
-        <div>
+        <div className="absolute top-20 ">
           <div>
             <h1>
-              Gabriel <span>Andrade</span>
+              <a
+                href="https://github.com/Suntzu1000"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span>Gabriel</span>
+              </a>
             </h1>
-            <div className="mb-6 text-[36px] " >
-              <span className='mr-4' >Eu sou um</span>
+            <div className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1] ">
+              <span className=" text-white mr-4">
+                Eu sou um Desenvolvedor JavaScript
+              </span>
               <TypeAnimation
-                sequence={[
-                  "Desenvolvedor JavaScript",
-                  2000,
-                  "ReactJS",
-                  2000,
-                  "NextJS",
-                  2000,
-                  "NodeJS",
-                ]}
+                sequence={["ReactJS", 2000, "NextJS", 2000, "NodeJS"]}
                 speed={50}
-                className='text-accent'
+                className="text-accent"
                 wrapper="span"
                 repeat={Infinity}
               />
             </div>
+            <p>
+              Tenho 18 anos, sou desenvolvedor de sistemas e estou focado na
+              linguagem JavaScript, entendendo frameworks como React, Node,
+              Next e aprendendo React-Native. Sou um apaixonado por tecnologias
+              e movido á desafios!
+            </p>
+            <button className="btn btn-lg" >Contrate-me</button>
+            <Link to="services" className="text-gradient btn-link" > <a href="">Meu Portfólio</a> </Link>
           </div>
 
           <div>
