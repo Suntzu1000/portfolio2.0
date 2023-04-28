8ff39064211ef1089b2f2d277ba8f9ec5fceadfc
Gabriel Andrade <99212400+Suntzu1000@users.noreply.github.com>
2023-04-28 14:54:10 -0300

    Alterando Header

--- portfolio-starter/src/components/Header.js
+++ portfolio-starter/src/components/Header.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { Link } from "react-scroll";
+import Logo from "../assets/Logo.png"
 
 const Header = () => {
   return (
-    <header className=" py-5 bg-gradient-to-r from-red-400 to-red-700 ">
+    <header className=" py-5 bg-gradient-to-r">
       <div className="container mx-auto">
         <div className="flex justify-between items-center ">
           <a href="home">
-            <h1 className="text-5xl font-bold xs:text-center xs:justify-items-center  ">Portfólio</h1>
+            <header className="text-5xl font-bold xs:text-center xs:justify-items-center  "> <img src={Logo} alt="Logo" /> </header>
           </a>
-          <Link to="contact" activeClass="active" smooth={true} spy={true}>
-            <button className="btn btn-sm xs:text-block hidden sm:block">Contrate-me </button>
-          </Link>
+            <button className="btn btn-sm xs:text-block hidden sm:block"> <a
+                  href="https://wa.me/62995396918"
+                  target="_blank"
+                  rel="noopener noreferrer">Contrate-me </a> </button>
         </div>
       </div>
     </header>
